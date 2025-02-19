f10d6e8dd0719af2da3e2d90e2e6c1f4c3117d8c
Efty Sifakis <esifakis@nvidia.com>
2025-02-19 14:25:34 -0600

    fixes

--- StudyGroupExamples/14Feb2025/include/Kernels.h
+++ StudyGroupExamples/14Feb2025/include/Kernels.h
@@ -1,16 +1,3 @@
 #pragma once
 
-#if 0
-float convolution(unsigned int leafCount,
-                  nanovdb::NanoGrid<nanovdb::ValueIndex> *grid,
-                  float *outTensor,
-                  const float *inTensor,
-                  const float *stencil_ptr);
-
-float stencilConvolveLauncher(
-    nanovdb::NanoGrid<nanovdb::ValueOnIndex> *hostGrid, nanovdb::NanoGrid<nanovdb::ValueOnIndex> *deviceGrid,
-    float *inputBuffer,
-    float *stencil,
-    float *outputBuffer);
-#endif
 float computeLaplacianGPU( float *uRaw, float *LuRaw);
